cfd24307c02d0bca9270ada2bc3695d2d5c36c77
OganKuba <kubog123@gmail.com>
2023-09-10 09:56:55 +0200

    End of section 2

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,19 @@
+using AuctionService.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data
+{
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Auction> Auctions{get; set;}
+
+        public static implicit operator ControllerContext(AuctionDbContext v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
